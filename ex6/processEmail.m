9f2348ef6d82cd3a54472aeebb5093f41767bab4
Jason C. Myers <myer0154@umn.edu>
2017-12-08 11:34:22 -0600

    Complete ex6

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,7 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
+    [inVocab, idx] = ismember(str, vocabList);
+    if (inVocab)
+      word_indices = [word_indices; idx];
+    end
 
 
 
